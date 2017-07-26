6017bc869141367ca815a7264fef9cfd770ff689
Jack Szwergold <JackSzwergold@gmail.com>
2017-07-26 13:45:25 -0400

    Getting some jQuery logic in place to handle vertical (aka: Y) positioning.

--- scrolling_text.js
+++ scrolling_text.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 
-  var widthBody = document.body.clientWidth
-  var widthHeight = document.body.clientHeight;
+  // Create a random array range.
+  randomRange = $.map(Array(10), function(value, index) {
 
-  var randomNumber = Math.floor(Math.random() * 90) + 10;
+    if (index > 0) {
+      return index * 10;
+    }
 
-  alert ('top: ' + randomNumber + '% : ' + widthBody + ' x ' + widthHeight);
+  })
+
+  $( "article > div" ).each(function(index) {
+
+    // Method 1: Basic random number. Sloppy and introduces duplicates.
+    // var randomNumber = (Math.floor(Math.random() * 9) + 1) * 10;
+    // Method 2: Now pick a random number. The 'randomRange' with splice allows non-duplicated random values.
+    var randomPosition = Math.floor(Math.random() * randomRange.length);
+    var randomNumber = randomRange.splice(randomPosition, 1);
+
+    $(this).css('top', randomNumber + '%');
+
+  });
+
 
 });
