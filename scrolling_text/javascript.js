fa7c11cb65e0dcfa914d97ebfac3329a8532ea47
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-15 10:11:23 -0400

    Experimenting with different ideas to randomize animated text.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   // Create a random array range.
-  randomRange = $.map(Array(10), function(value, index) {
+  randomPositionRange = $.map(Array(10), function(value, index) {
 
     if (index > 0) {
       return index * 10;
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
   $( "article > div" ).each(function(index) {
 
-    // Pick a random number. The 'randomRange' with splice allows non-duplicated random values.
-    var randomPosition = Math.floor(Math.random() * randomRange.length);
-    var randomNumber = randomRange.splice(randomPosition, 1);
+    // Pick a random number. The 'randomPositionRange' with splice allows non-duplicated random values.
+    var randomPosition = Math.floor(Math.random() * randomPositionRange.length);
+    var randomNumber = randomPositionRange.splice(randomPosition, 1);
     $(this).css('top', randomNumber + '%');
 
     // Method 1: Basic random number. Sloppy and introduces duplicates.
