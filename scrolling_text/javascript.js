a81d2dd7c2f57fda18b6b39e0a0df8a26f37481b
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-15 10:37:13 -0400

    Refining the JavaScript.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
 
   // Create a random array range.
-  randomPositionRange = $.map(Array(10), function(value, index) {
+  randomPositionRange = $.map(Array(20), function(value, index) {
 
     if (index > 0) {
-      return index * 10;
+      return index * 5;
     }
 
   })
 
   $( "article > div" ).each(function(index) {
 
-    // Pick a random position.
+    // Pick a random position from the preset range of values.
     var randomPosition = Math.floor(Math.random() * randomPositionRange.length);
-    var randomNumber = randomPositionRange.splice(randomPosition, 1);
+    var randomNumber = randomPositionRange.splice(randomPosition, 1)[0];
+    console.log(randomNumber);
     $(this).css('top', randomNumber + '%');
 
-    // Pick a random duration.
+    // Pick a random duration value.
     var minDuration = 8;
     var maxDuration = 18;
     var randomDuration = (Math.floor(Math.random() * (maxDuration - minDuration)) + minDuration);
-    console.log(randomDuration);
+    // console.log(randomDuration);
     $(this).css('animation-duration', randomDuration + 's');
 
   });
