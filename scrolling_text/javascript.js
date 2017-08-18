c00e96d8fc4ffffeb0e6696e38cb6935794211fe
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-18 16:49:17 -0400

    Playing around with some ideas for the scrolling text.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -7,24 +7,33 @@ $(document).ready(function() {
       return index * 5;
     }
 
-  })
+  });
 
-  $( "article > div" ).each(function(index) {
+  $( "article > div.animate" ).each(function(index) {
+
+    var element = $(this);
 
     // Pick a random position from the preset range of values.
     var randomPosition = Math.floor(Math.random() * randomPositionRange.length);
     var randomNumber = randomPositionRange.splice(randomPosition, 1)[0];
     console.log(randomNumber);
-    $(this).css('top', randomNumber + '%');
+    element.css('top', randomNumber + '%');
 
     // Pick a random duration value.
     var minDuration = 8;
     var maxDuration = 18;
     var randomDuration = (Math.floor(Math.random() * (maxDuration - minDuration)) + minDuration);
     // console.log(randomDuration);
-    $(this).css('animation-duration', randomDuration + 's');
+    element.css('animation-duration', randomDuration + 's');
+
+    // element.bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
+    //   element.replaceWith(element.clone(true));
+    // });
 
   });
 
+  // $( "article > div.animate" ).bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
+  //   element.replaceWith(element.clone(true));
+  // });
 
 });
