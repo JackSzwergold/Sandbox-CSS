924e824daa3e1c84d745a2ba15e1210979dd254d
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-18 21:52:02 -0400

    Refactoring some of the JavaScript for the scrolling text.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
 
-  randomRange = $.map(Array(20), function(value, index) {
-    if (index > 0) {
-      return index * 5;
-    }
-  });
-
+  // A function to calculate a random position value.
   function randomPosition() {
+
+    // Generate a random range.
+    randomRange = $.map(Array(20), function(value, index) {
+      if (index > 0) {
+        return index * 5;
+      }
+    });
+
+    // Pic a random value out of a random range.
     var randomRangeKey = Math.floor(Math.random() * randomRange.length);
     return randomRange.splice(randomRangeKey, 1)[0];
-  }
 
+  } //randomPosition
+
+  // A function to calculate a random duration value.
   function randomDuration() {
-    var minDuration = 8;
-    var maxDuration = 18;
-    return (Math.floor(Math.random() * (maxDuration - minDuration)) + minDuration);
-  }
 
+    var min = 8;
+    var max = 18;
+
+    return (Math.floor(Math.random() * (max - min)) + min);
+
+  } // randomDuration
+
+  // Set random values for each element.
   $("article > div.animate").each(function(index) {
     var element = $(this);
     element.css('top', randomPosition() + '%');
     element.css('animation-duration', randomDuration() + 's');
-    // element.bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
-    //   element.replaceWith(element.clone(true));
-    // });
+  });
+
+  // Detect when the animation ends and then restart it.
+  $("article > div.animate").bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
+    var element = $(this);
+    element.replaceWith(element.clone(true));
   });
 
 });
