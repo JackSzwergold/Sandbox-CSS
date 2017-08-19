ed11d3ff78a5d697fce59d7fee0b0a23e515ca4f
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-18 22:37:29 -0400

    Cleaning up code and more refactoring of JavaScript.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -17,26 +17,26 @@ $(document).ready(function() {
   } //randomPosition
 
   // A function to calculate a random duration value.
-  function randomValue() {
+  function randomValue(min, max) {
 
-    // Set the min and max values.
-    var min = 1;
-    var max = 7;
+    // Set default values.
+    min = (typeof min !== 'undefined') ? min : 1;
+    max = (typeof max !== 'undefined') ? max : 7;
 
-    // Return the randome values.
+    // Return the random values.
     return (Math.floor(Math.random() * (max - min)) + min);
 
   } // randomValue
 
   // Set random values for each element.
   $("article > div.animate").each(function(index) {
-    $(this).css({'top': randomPosition() + '%', 'animation-duration': randomValue() + 's'});
+    $(this).css({'top': randomPosition() + '%', 'animation-duration': randomValue() + 's', 'animation-delay': randomValue(0.25,1) + 's'});
   });
 
   // Detect when the animation ends and then restart it.
   $("article > div.animate").bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
     var element = $(this);
-    var new_values = {'top': randomPosition() + '%', 'animation-duration': randomValue() + 's'}
+    var new_values = {'top': randomPosition() + '%', 'animation-duration': randomValue() + 's', 'animation-delay': randomValue(0.25,1) + 's'}
     element.replaceWith(element.clone(true).css(new_values));
   });
 
