d195b4a7dc936340cb1f2c1857822961cc36d136
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-18 21:38:10 -0400

    Refactoring some of the JavaScript for the scrolling text.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -1,39 +1,29 @@
 $(document).ready(function() {
 
-  // Create a random array range.
-  randomPositionRange = $.map(Array(20), function(value, index) {
-
+  randomRange = $.map(Array(20), function(value, index) {
     if (index > 0) {
       return index * 5;
     }
-
   });
 
-  $( "article > div.animate" ).each(function(index) {
+  function randomPosition() {
+    var randomRangeKey = Math.floor(Math.random() * randomRange.length);
+    return randomRange.splice(randomRangeKey, 1)[0];
+  }
 
-    var element = $(this);
-
-    // Pick a random position from the preset range of values.
-    var randomPosition = Math.floor(Math.random() * randomPositionRange.length);
-    var randomNumber = randomPositionRange.splice(randomPosition, 1)[0];
-    console.log(randomNumber);
-    element.css('top', randomNumber + '%');
-
-    // Pick a random duration value.
+  function randomDuration() {
     var minDuration = 8;
     var maxDuration = 18;
-    var randomDuration = (Math.floor(Math.random() * (maxDuration - minDuration)) + minDuration);
-    // console.log(randomDuration);
-    element.css('animation-duration', randomDuration + 's');
+    return (Math.floor(Math.random() * (maxDuration - minDuration)) + minDuration);
+  }
 
+  $("article > div.animate").each(function(index) {
+    var element = $(this);
+    element.css('top', randomPosition() + '%');
+    element.css('animation-duration', randomDuration() + 's');
     // element.bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
     //   element.replaceWith(element.clone(true));
     // });
-
   });
-
-  // $( "article > div.animate" ).bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
-  //   element.replaceWith(element.clone(true));
-  // });
 
 });
