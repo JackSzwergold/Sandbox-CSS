941bb60d4bfac55ebc78d9d9105b9e9dcb8a8000
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-21 13:00:07 -0400

    Updating the JavaScript.

--- scrolling_text/javascript.js
+++ scrolling_text/javascript.js
@@ -1,18 +1,46 @@
 $(document).ready(function() {
 
+  // Assign the values of the searched for elements to a variable.
+  var items = $("article > div");
+
+  // Assign the values of the searched for elements to a variable.
+  var randomRangeY = generateRangeY(items.length);
+
+  /****************************************************************************/
+
   // A function to calculate a random position value.
-  function randomPosition() {
+  function generateRangeY(item_count) {
+
+    // Set default values.
+    var item_count = (typeof item_count !== 'undefined') ? item_count : 10;
+
+    // We multiply the count by 3 to give the positioning range more values.
+    item_count = item_count * 3;
+
+    // Set an item value based on the count divided by 100%.
+    var item_value = Math.round(100 / item_count);
 
     // Generate a random range.
-    randomRange = $.map(Array(11), function(value, index) {
+    return $.map(Array(item_count), function(value, index) {
       if (index > 0) {
-        return index * 8;
+        return index * item_value;
       }
     });
 
+  } // generateRangeY
+
+  // A function to calculate a random position value.
+  function randomPosition(item_count) {
+
+    if (randomRangeY.length <= 3) {
+      randomRangeY = generateRangeY(item_count);
+    }
+
     // Pic a random value out of a random range.
-    var randomRangeKey = Math.floor(Math.random() * randomRange.length);
-    return randomRange.splice(randomRangeKey, 1)[0];
+    var randomRangeKeyY = Math.floor(Math.random() * randomRangeY.length);
+
+    // Return a random range value.
+    return randomRangeY.splice(randomRangeKeyY, 1)[0];
 
   } //randomPosition
 
@@ -20,24 +48,37 @@ $(document).ready(function() {
   function randomValue(min, max) {
 
     // Set default values.
-    min = (typeof min !== 'undefined') ? min : 3;
-    max = (typeof max !== 'undefined') ? max : 18;
+    var min = (typeof min !== 'undefined') ? min : 3;
+    var max = (typeof max !== 'undefined') ? max : 18;
 
     // Return the random values.
     return (Math.floor(Math.random() * (max - min)) + min);
 
   } // randomValue
 
+  /****************************************************************************/
+
   // Set random values for each element.
-  $("article > div").each(function(index) {
-    $(this).css({'top': randomPosition() + '%', 'animation-duration': randomValue() + 's', 'animation-delay': randomValue(0.125,5) + 's'});
+  items.each(function() {
+
+    // Assign the element to a variable.
+    var element = $(this);
+
+    // Set initial values to the animation.
+    element.css({'top': randomPosition(items.length) + '%', 'animation-duration': randomValue() + 's', 'animation-delay': randomValue(0.125, 5) + 's'});
+
+    // Detect when the animation ends and then restart it.
+    element.bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
+
+      // Set new values.
+      var new_values = {'top': randomPosition(items.length) + '%', 'animation-duration': randomValue() + 's', 'animation-delay': randomValue(0.125, 5) + 's'}
+
+      // Clone and replace the element to restart the WebKit animation.
+      $(this).replaceWith($(this).clone(true).css(new_values));
+
+    });
+
   });
 
-  // Detect when the animation ends and then restart it.
-  $("article > div").bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function() {
-    var element = $(this);
-    var new_values = {'top': randomPosition() + '%', 'animation-duration': randomValue() + 's', 'animation-delay': randomValue(0.125,5) + 's'}
-    element.replaceWith(element.clone(true).css(new_values));
-  });
 
 });
