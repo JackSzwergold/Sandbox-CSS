79125b86941a41450909fa5c0d09706819197dee
Jack Szwergold <JackSzwergold@gmail.com>
2017-08-28 12:05:01 -0400

    First attempt to add some CSS to the mix.

--- preworn_css/javascript.js
+++ preworn_css/javascript.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   $('input').change(function(){
-    $(this).delay(1800).queue(function() {
-      // if ($(this).is(':checked')) {}
-      // $(this).prop('checked', false).dequeue();
-    });
+    // $(this).delay(300).queue(function() {
+    //   // if ($(this).is(':checked')) {}
+    //   // $(this).prop('checked', false).dequeue();
+    // });
   });
 
 });
