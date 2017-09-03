2e0e5b600fa2e74f8bfb260d2e675049ccf2195a
Jack Szwergold <JackSzwergold@gmail.com>
2017-09-03 00:52:02 -0400

    Merging test stuff into the 'Carousel Pentagon' stuff.

--- carousel_pentagon/javascript.js
+++ carousel_pentagon/javascript.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-  $('input').change(function(){
-    $(this).delay(1800).queue(function() {
-      // if ($(this).is(':checked')) {}
-      $(this).prop('checked', false).dequeue();
-    });
-  });
+  // $('div.wrapper [class*="item_"]').change(function(){
+  //   $(this).delay(1800).queue(function() {
+  //     // if ($(this).is(':checked')) {}
+  //     $(this).prop('checked', false).dequeue();
+  //   });
+  // });
 
 });
