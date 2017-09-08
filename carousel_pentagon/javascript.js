0aeb092622a2a4f772f6c6676585e1089075c4be
Jack Szwergold <JackSzwergold@gmail.com>
2017-09-08 13:03:54 -0400

    Playing around with JavaScript functions to get some automatted stuff in place.

--- carousel_pentagon/javascript.js
+++ carousel_pentagon/javascript.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
 
-  // $('div.wrapper [class*="item_"]').change(function(){
-  //   $(this).delay(1800).queue(function() {
-  //     // if ($(this).is(':checked')) {}
-  //     $(this).prop('checked', false).dequeue();
-  //   });
-  // });
+  // This de-selects a checked element after a predetermined period of time.
+  function deselectElement(element_selector) {
+    $(element_selector).change(function(){
+      $(this).delay(1800).queue(function() {
+        $(this).prop("checked", false).dequeue();
+      });
+    });
+  }
+
+  // This iterates through the list of inputs and checks each element.
+  function iterateElements(element_selector) {
+    var iteration = 900;
+    var delay = iteration;
+    $(element_selector).each(function(index) {
+      var element = $(this);
+      setTimeout( function(){
+        $(element).prop("checked", true);
+      }, delay);
+      delay += iteration;
+    }).promise().done(function() {
+      setTimeout(function() {
+        iterateElements(element_selector);
+      }, delay += iteration);
+    });
+  }
+
+  // Different ways to call the functions.
+  // deselectElement('div.wrapper [id*="element_"]');
+  iterateElements('div.wrapper input[id*="element_"]');
+  // iterateElements('body input[id*="wrapper_"]');
 
 });
