94af8b5545189ee1053217d75af287623065e3fb
Jack Szwergold <JackSzwergold@gmail.com>
2017-09-11 16:15:26 -0400

    Some overall JavaScript and CSS  tweaks and such.

--- carousel_pentagon/javascript.js
+++ carousel_pentagon/javascript.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
 
   // This de-selects a checked element after a predetermined period of time.
-  function deselectElement(element_selector) {
+  function deselectElement(element_selector, delay) {
+    var delay = ((typeof delay !== 'undefined') ? delay : 1800);
     $(element_selector).change(function(){
-      $(this).delay(1800).queue(function() {
+      $(this).delay(delay).queue(function() {
         $(this).prop("checked", false).dequeue();
       });
     });
   }
 
   // This iterates through the list of inputs and checks each element.
-  function iterateElements(element_selector) {
-    var iteration = 900;
+  function iterateElements(element_selector, iteration) {
+    var iteration = ((typeof iteration !== 'undefined') ? iteration : 900);
     var delay = iteration;
     $(element_selector).each(function(index) {
       var element = $(this);
@@ -21,14 +22,15 @@ $(document).ready(function() {
       delay += iteration;
     }).promise().done(function() {
       setTimeout(function() {
-        iterateElements(element_selector);
-      }, delay += iteration);
+        iterateElements(element_selector, iteration);
+      }, delay -= iteration);
     });
   }
 
   // Different ways to call the functions.
-  // deselectElement('div.wrapper [id*="element_"]');
-  // iterateElements('div.wrapper input[id*="element_"]');
-  // iterateElements('body input[id*="wrapper_"]');
+  // deselectElement('div.wrapper [id*="element_"]', 600);
+  // iterateElements('div.wrapper input[type=checkbox][id*="element_"]', 600);
+  // iterateElements('div.wrapper input[type=radio][id*="element_"]', 600);
+  // iterateElements('body input[type=checkbox][id*="wrapper_"]', 1800);
 
 });
--- cube_face/javascript.js
+++ cube_face/javascript.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
 
-  $('input').change(function(){
-    $(this).delay(1800).queue(function() {
-      // if ($(this).is(':checked')) {}
-      $(this).prop('checked', false).dequeue();
+  // This de-selects a checked element after a predetermined period of time.
+  function deselectElement(element_selector, delay) {
+    var delay = ((typeof delay !== 'undefined') ? delay : 1800);
+    $(element_selector).change(function(){
+      $(this).delay(delay).queue(function() {
+        $(this).prop("checked", false).dequeue();
+      });
     });
-  });
+  }
+
+  // This iterates through the list of inputs and checks each element.
+  function iterateElements(element_selector, iteration) {
+    var iteration = ((typeof iteration !== 'undefined') ? iteration : 900);
+    var delay = iteration;
+    $(element_selector).each(function(index) {
+      var element = $(this);
+      setTimeout( function(){
+        $(element).prop("checked", true);
+      }, delay);
+      delay += iteration;
+    }).promise().done(function() {
+      setTimeout(function() {
+        iterateElements(element_selector, iteration);
+      }, delay -= iteration);
+    });
+  }
+
+  // Different ways to call the functions.
+  deselectElement('div.wrapper [id*="element_"]', 600);
+  // iterateElements('div.wrapper input[type=checkbox][id*="element_"]', 600);
+  // iterateElements('div.wrapper input[type=radio][id*="element_"]', 600);
+  // iterateElements('body input[type=checkbox][id*="wrapper_"]', 1800);
 
 });
