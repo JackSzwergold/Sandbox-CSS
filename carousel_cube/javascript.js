1c48abedf176cf82c6e949dba25de13b1e9e9236
Jack Szwergold <JackSzwergold@gmail.com>
2017-09-14 20:41:44 -0400

    Integrating the touch event stuff into the 'Carousel Cube' and 'Carousel Pentagon' stuff.

--- carousel_cube/javascript.js
+++ carousel_cube/javascript.js
@@ -33,4 +33,57 @@ $(document).ready(function() {
   // iterateElements('div.wrapper input[type=radio][id*="element_"]', 600);
   // iterateElements('body input[type=checkbox][id*="wrapper_"]', 1800);
 
+  //////////////////////////////////////////////////////////////////////////////
+  // TODO: Touch tests.
+
+  // Set some variables.
+  // var elements_count = $('div.wrapper input[type=radio][id*="element_"]');
+  var elements_count = $('div.wrapper div.container > div.element');
+  var count = 0;
+  // var limit = elements_count.length + 1;
+  var limit = 5;
+
+  var hammer_container = new Hammer($('div.container')[0]);
+  hammer_container.on('tap', function(event) {
+
+    // Increment.
+    count = (count + 1) % limit;
+    count = count == 0 ? count += 1 : count;
+
+    // Decrement.
+    // count = (count + limit - 1) % limit;
+    // count = count == 0 ? count = (limit - 1) : count;
+
+    var element = $('div.wrapper input[type=radio][id="element_' + count + '"]');
+    $(element).prop('checked', !$(element).attr('checked'));
+    console.log(elements_count.length + ' | ' + event.type + ' | ' + count + ' | ' + limit);
+  });
+
+  // var hammer_wrapper = new Hammer($('div.wrapper')[0]);
+  // hammer_wrapper.on('tap', function(event) {
+  //   var element = $('div.wrapper input[type=checkbox][id*="element_"]');
+  //   $(element).prop('checked', !$(element).attr('checked'));
+  //   // console.log(event.type);
+  // });
+
+  // var hammer_container = new Hammer($('div.container')[0]);
+  // // var hammer_container = new Hammer($('div.element')[0]);
+  // hammer_container.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
+
+  // hammer_container.on('swipeleft', function(event) {
+  //   var element_selector = 'div.wrapper input[type=radio][id="element_1"]';
+  //   var element = $(element_selector);
+  //   $(element).prop('checked', !$(element).attr('checked'));
+  //   console.log(event.target.className + ' - ' + event.type);
+  // });
+
+  // hammer_container.on('swiperight', function(event) {
+  //   var element_selector = 'div.wrapper input[type=radio][id="element_2"]';
+  //   var element = $(element_selector);
+  //   var elements_count = $('div.wrapper input[type=radio][id*="element_"]');
+  //   console.log(elements_count.length);
+  //   $(element).prop('checked', !$(element).attr('checked'));
+  //   console.log(event.target.className + ' - ' + event.type);
+  // });
+
 });
