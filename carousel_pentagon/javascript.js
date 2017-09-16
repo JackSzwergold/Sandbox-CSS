c226d66f903aea4cbc255ffdca462ba7f4799c4f
Jack Szwergold <JackSzwergold@gmail.com>
2017-09-16 00:28:13 -0400

    Some JavaScript futzing.

--- carousel_pentagon/javascript.js
+++ carousel_pentagon/javascript.js
@@ -58,6 +58,7 @@ $(document).ready(function() {
   var element_index = 1;
   // var limit = $('div.wrapper input[type=radio][id*="element_"]').length;
   var limit = $('div.wrapper div.container > div.element').length + 1;
+  var direction = true;
 
   // Init Hammer.
   var hammer_container = new Hammer($('div.wrapper div.container')[0]);
@@ -72,12 +73,25 @@ $(document).ready(function() {
     // Determine the the index value of the clicked element.
     var element_index = $(event.target).closest('div.element').index();
 
-    if (event.type == 'tap') {
-      selected_index = Math.abs(element_index + 1);
-    }
-    else if (event.type == 'press') {
-      selected_index = newIndexValue(selected_index, limit, true);
-    }
+
+    $('div.wrapper').click(function(event) {
+      direction = event.pageX >= ($(this).offset().left + $(this).width()/2) ? false : true;
+      console.log(direction);
+    });
+
+    // selected_index = newIndexValue(selected_index, limit, direction);
+
+
+    // alert(event.pageX >= ($(this).offset().left + $(this).width()/2) ? 'right' : 'left');
+
+    // if (event.type == 'tap') {
+    //   selected_index = Math.abs(element_index + 1);
+    // }
+    // else if (event.type == 'press') {
+    //   selected_index = newIndexValue(selected_index, limit, direction);
+    // }
+
+
 
     // Determine the control elemement for the clicked element.
     var control_element = $('div.wrapper input[type=radio][id="element_' + selected_index + '"]');
@@ -86,7 +100,7 @@ $(document).ready(function() {
     control_element.prop('checked', !$(control_element).attr('checked'));
 
     // Log stuff for debugging.
-    console.log(event.type + ' | element_index: ' + element_index + ' | selected_index: ' + selected_index + ' | limit: ' + limit);
+    // console.log(event.type + ' | element_index: ' + element_index + ' | selected_index: ' + selected_index + ' | limit: ' + limit);
 
   });
 
