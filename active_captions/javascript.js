a65cf146b41ac2bce97f80f0351c886df2889b69
Jack Szwergold <JackSzwergold@gmail.com>
2017-10-06 23:59:59 -0400

    Removing unused stuff.

--- active_captions/javascript.js
+++ active_captions/javascript.js
@@ -1,36 +1,3 @@
 $(document).ready(function() {
 
-  // This de-selects a checked element after a predetermined period of time.
-  function deselectElement(element_selector, delay) {
-    var delay = ((typeof delay !== 'undefined') ? delay : 1800);
-    $(element_selector).change(function(){
-      $(this).delay(delay).queue(function() {
-        $(this).prop("checked", false).dequeue();
-      });
-    });
-  }
-
-  // This iterates through the list of inputs and checks each element.
-  function iterateElements(element_selector, iteration) {
-    var iteration = ((typeof iteration !== 'undefined') ? iteration : 900);
-    var delay = iteration;
-    $(element_selector).each(function(index) {
-      var element = $(this);
-      setTimeout( function(){
-        $(element).prop('checked', !$(element).attr('checked'));
-      }, delay);
-      delay += iteration;
-    }).promise().done(function() {
-      setTimeout(function() {
-        iterateElements(element_selector, iteration);
-      }, delay -= iteration);
-    });
-  }
-
-  // Different ways to call the functions.
-  deselectElement('div.wrapper [id*="element_"]', 600);
-  // iterateElements('div.wrapper input[type=checkbox][id*="element_"]', 600);
-  // iterateElements('div.wrapper input[type=radio][id*="element_"]', 600);
-  // iterateElements('body input[type=checkbox][id*="wrapper_"]', 1800);
-
 });
